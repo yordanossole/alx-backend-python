cf98c0f3565d04950e3f723c539b295e1b46c02a
yordanossole <yordanos.sole@gmail.com>
2025-05-25 06:15:43 +0300

    0-log_queries.py

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,21 @@
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+ """ YOUR CODE GOES HERE"""
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
+
+def log_queries():
+    return
