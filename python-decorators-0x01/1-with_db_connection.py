ee727d33d6ddbc105b91f00506dc5c0ca0ea608a
yordanossole <yordanos.sole@gmail.com>
2025-05-25 06:21:22 +0300

    1-with_db_connection.py

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -3,12 +3,13 @@ import functools
 
 def with_db_connection(func):
     # """ your code goes here""" 
+    return
 
-@with_db_connection 
+# @with_db_connection 
 def get_user_by_id(conn, user_id): 
-cursor = conn.cursor() 
-cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,)) 
-return cursor.fetchone() 
+    cursor = conn.cursor() 
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,)) 
+    return cursor.fetchone() 
 #### Fetch user by ID with automatic connection handling 
 
 user = get_user_by_id(user_id=1)
