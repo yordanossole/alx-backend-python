03422296315d49f11817ee6bcda1b15233e33dc2
yordanossole <yordanos.sole@gmail.com>
2025-05-25 06:43:12 +0300

    3-concurrent.py

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -6,5 +6,5 @@ def async_fetch_older_users():
 
 ["import aiosqlite", "connect", "execute"]
 ["async def async_fetch_users()", "async def async_fetch_older_users()", "await"]
-["asyncio.gather()"]
+["await asyncio.gather"]
 ["asyncio.run(fetch_concurrently())"]
