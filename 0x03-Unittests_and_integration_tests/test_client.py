9aa8495288e3052422e84e15ec1a9007f9010876
yordanossole <yordanos.sole@gmail.com>
2025-06-01 15:24:18 +0300

    test_client.py update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -66,6 +66,14 @@ class TestGithubOrgClient(unittest.TestCase):
                 "https://fakeurl.com/orgs/testorg/repos"
             )
 
+    @parameterized.expand([
+            ({"license": {"key": "my_license"}}, "my_license", True),
+            ({"license": {"key": "other_license"}}, "my_license", False),
+        ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test has_license returns expected boolean for different inputs"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
 
 if __name__ == "__main__":
     unittest.main()
